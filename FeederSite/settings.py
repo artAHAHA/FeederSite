bdf54eb824ae901881ad5f948a33516192df9249
Artur <[belokurovarturka@gmail.com])>
2024-03-01 23:30:30 +0300

    href styles

--- FeederSite/settings.py
+++ FeederSite/settings.py
@@ -125,9 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATICFILES_DIRS = [BASE_DIR / 'main' / 'static']
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'main/static', 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
